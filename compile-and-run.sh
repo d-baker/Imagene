a1d48a6d5e503a52d9d633d55ed75cc43d3bc7a6
dbaker <d-_-baker@hotmail.com>
2016-11-26 15:01:53 +1100

    add bash script to compile and run without needing an IDE

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+#######################################
+# Written by Dorothea Baker (s3367422)
+# for
+# Programming Project 1
+# SP3 2016
+#######################################
+
+# This script is intended for those who wish to compile and run the project without using an IDE. It should work on any unix operating system.
+
 # compile all classes
 find -name "*.java" > sources.txt
 javac -cp .:lib/* @sources.txt
@@ -16,3 +25,4 @@ java -cp ../Imagene.jar:../lib/* imagene.view.Main
 
 # To remove compiled class files when you're done with them, execute:
 # git clean -f *.class
+# (Note: this will only work if you cloned the github repo for the project and have git installed)
